555a7ae303f9fc9c14e194b114b5d3788d9318a1
calvin hendrawan <calvin.h.ft21@mail.umy.ac.id>
2023-01-22 18:57:33 +0700

    Add project files.

---
+++ UAS_20210140138/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAS_20210140138
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
